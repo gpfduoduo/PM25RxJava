17af2cfc3c49a281af9bbece4d63d9f1d3a3ead3
郭攀峰 <guopanfeng.123456@163.com>
2015-12-19 01:47:34 +0800

    完成启动页面的动画类似于微博

--- app/src/main/java/com/guo/duoduo/pm25rxjava/ui/SplashActivity.java
+++ app/src/main/java/com/guo/duoduo/pm25rxjava/ui/SplashActivity.java
@@ -9,6 +9,10 @@ import rx.android.schedulers.AndroidSchedulers;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.AnimationSet;
+import android.view.animation.TranslateAnimation;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.guo.duoduo.pm25rxjava.R;
@@ -18,7 +22,9 @@ public class SplashActivity extends AppCompatActivity
 {
 
     private TextView tvSplash;
+    private ImageView imgSplash;
     private static final int DELAY = 2;
+    private static final int ANIMATION = DELAY * 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -52,6 +58,24 @@ public class SplashActivity extends AppCompatActivity
 
     private void initWidget()
     {
+        AlphaAnimation alphaAnimation = new AlphaAnimation(0, 1);
+        alphaAnimation.setDuration(ANIMATION);
+        TranslateAnimation animation = new TranslateAnimation(0, 0, 0, -150);
+        animation.setDuration(ANIMATION);
+        AnimationSet set = new AnimationSet(false);
+        set.addAnimation(alphaAnimation);
+        set.addAnimation(animation);
+        set.setFillAfter(true);
         tvSplash = (TextView) findViewById(R.id.tv_splash);
+        tvSplash.startAnimation(set);
+        imgSplash = (ImageView) findViewById(R.id.img_splash);
+        imgSplash.startAnimation(set);
+    }
+
+    public void onDestroy()
+    {
+        super.onDestroy();
+        tvSplash.clearAnimation();
+        imgSplash.clearAnimation();
     }
 }
